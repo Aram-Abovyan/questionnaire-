fcbf6da9e62aefe39c9f4fcbaec36859c48942c2
Aram <aram1993abovyan@gmail.com>
2021-09-20 19:57:06 +0400

    added styles to main page

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,5 +1,6 @@
 import './main-page.scss';
 import backgroundImage from '../../assets/images/main/background.svg';
+import routeArea from '../../assets/images/main/route-area.svg';
 import {
   binaryxSpaceship,
   digitalAgencySpaceship,
@@ -12,13 +13,25 @@ export const MainPage = () => {
   return (
     <>
       <img className="background-image" src={backgroundImage} alt="" />
-      
+
       <div className="main-page">
-        <img src={binaryxSpaceship} alt=""/>
-        <img src={digitalAgencySpaceship} alt=""/>
-        <img src={easeSpaceship} alt=""/>
-        <img src={foodexSpaceship} alt=""/>
-        <img src={powercodeSpaceship} alt=""/>
+        <img className="route-area" src={routeArea} alt="" />
+
+        <div className="spaceship-container">
+          <div>
+            <img src={foodexSpaceship} alt=""/>
+            <img src={powercodeSpaceship} alt=""/>
+          </div>
+
+          <div>
+            <img src={digitalAgencySpaceship} alt=""/>
+          </div>
+
+          <div>
+            <img src={binaryxSpaceship} alt=""/>
+            <img src={easeSpaceship} alt=""/>
+          </div>
+        </div>
       </div>
     </>
   );
